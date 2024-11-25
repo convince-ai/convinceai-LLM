46439fc3c3cbc55dac433852f6ca3f8528982b6b
Your Name <you@example.com>
2024-11-25 17:55:13 -0300

    Bug fixed when changing commiting products

--- readTxt.py
+++ readTxt.py
@@ -1,10 +1,10 @@
 def load_and_split_document(file_path):
-    with open('products.txt', "r", encoding="utf-8") as file:
+    with open(r'C:\Users\felip\OneDrive\Área de Trabalho\llaama\convinceai-LLM\products.txt', "r", encoding="utf-8") as file:
         document_text = file.read()
     sections = document_text.split("\n\n")
     return sections
 
 if __name__ == "__main__":
-    sections = load_and_split_document("produtos.txt")
+    sections = load_and_split_document(r"C:\Users\felip\OneDrive\Área de Trabalho\llaama\convinceai-LLM\products.txt")
     for section in sections:
         print(section, "\n")
